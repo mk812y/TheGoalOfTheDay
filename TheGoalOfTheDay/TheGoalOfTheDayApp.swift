2d25d8796baeec7fbcc5fa759c974e97e06323c2
mk812y <mk812y@gmail.com>
2023-01-18 19:37:53 +0300

    Initial Commit

---
+++ TheGoalOfTheDay/TheGoalOfTheDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheGoalOfTheDayApp.swift
+//  TheGoalOfTheDay
+//
+//  Created by Михаил Куприянов on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TheGoalOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
