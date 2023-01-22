e0a908d1bd330d9117f9426331821493f51cde74
mk812y <mk812y@gmail.com>
2023-01-22 21:29:04 +0300

    day 1

--- TheGoalOfTheDay/ContentView.swift
+++ TheGoalOfTheDay/ContentView.swift
@@ -5,17 +5,49 @@
 //  Created by Михаил Куприянов on 18.01.2023.
 //
 
+//Image(systemName: "globe")
+//    .imageScale(.large)
+//    .foregroundColor(.accentColor)
+
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+//            Color(.systemGray5)
+            VStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 5)
+                        .fill(Color.white)
+                        .frame(height: 150)
+                    .padding()
+                    VStack {
+                        HStack {
+                            Image(systemName: "tortoise.fill")
+                            Text("Duolingo")
+                        }
+                        .font(.title2)
+                        Text("Duolingo")
+                    }
+                }
+                HStack {
+                    Circle()
+                        .foregroundColor(Color("dust"))
+                    Circle()
+                        .foregroundColor(Color("peachDust"))
+                    Circle()
+                        .foregroundColor(Color("peach"))
+                    Circle()
+                        .foregroundColor(Color("greenDust"))
+                    Circle()
+                        .foregroundColor(Color("green"))
+                }
+            }
+            
+            
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
