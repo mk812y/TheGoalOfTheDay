0d90efa459b6c00817757798c998f87072572381
mk812y <mk812y@gmail.com>
2023-01-24 22:05:26 +0300

    пока так

--- TheGoalOfTheDay/ContentView.swift
+++ TheGoalOfTheDay/ContentView.swift
@@ -11,29 +11,46 @@
 
 
 import SwiftUI
+import Charts
+
+struct Job: Identifiable {
+    let id = UUID()
+    let job: String
+    let start: Double
+    let end: Double
+}
 
 struct ContentView: View {
+    @State private var progress = 0.6
+    
+    let data: [Job] = [
+        Job(job: "Job 1", start: 0, end: 15),
+        Job(job: "Job 2", start: 5, end: 25),
+        Job(job: "Job 1", start: 20, end: 35),
+        Job(job: "Job 1", start: 40, end: 55),
+        Job(job: "Job 2", start: 30, end: 60),
+        Job(job: "Job 2", start: 30, end: 60)
+    ]
+    
     var body: some View {
         ZStack {
             Color("1g")
             VStack {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 5)
-                        .fill(Color.white)
-                        .frame(height: 150)
+                Gauge(value: 15, in: 0...100) {}
+                    .gaugeStyle(.accessoryLinear)
+                    .tint(Gradient(colors: [.red, .yellow, .green]))
                     .padding()
-                    VStack {
-                        HStack {
-                            Image(systemName: "tortoise.fill")
-                            Text("Duolingo")
-                        }
-                        .font(.title2)
-                        Text("Duolingo")
-                    }
-                    CapsuleRectangle()
-                }
                 
-
+                Chart(data, id: \.job) {
+                    BarMark(
+                        xStart: .value("Start Time", $0.start),
+                        xEnd: .value("End Time", $0.end),
+                        y: .value("Job", $0.job)
+                    )
+                }
+                .frame(height: 200)
+                .padding()
+                
             }
             
             
