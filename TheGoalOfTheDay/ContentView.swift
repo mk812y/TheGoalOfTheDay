2c9a3241b0456efa98c5783b4c1202aeb45e5d73
mk812y <mk812y@gmail.com>
2023-01-25 21:16:10 +0300

    remove charting

--- TheGoalOfTheDay/ContentView.swift
+++ TheGoalOfTheDay/ContentView.swift
@@ -8,6 +8,7 @@
 //Image(systemName: "globe")
 //    .imageScale(.large)
 //    .foregroundColor(.accentColor)
+//
 
 
 import SwiftUI
@@ -21,37 +22,12 @@ struct Job: Identifiable {
 }
 
 struct ContentView: View {
-    @State private var progress = 0.6
-    
-    let data: [Job] = [
-        Job(job: "Job 1", start: 0, end: 15),
-        Job(job: "Job 2", start: 5, end: 25),
-        Job(job: "Job 1", start: 20, end: 35),
-        Job(job: "Job 1", start: 40, end: 55),
-        Job(job: "Job 2", start: 30, end: 60),
-        Job(job: "Job 2", start: 30, end: 60)
-    ]
+    @State private var progress = 0.6    
     
     var body: some View {
         ZStack {
             Color("1g")
-            VStack {
-                Gauge(value: 15, in: 0...100) {}
-                    .gaugeStyle(.accessoryLinear)
-                    .tint(Gradient(colors: [.red, .yellow, .green]))
-                    .padding()
-                
-                Chart(data, id: \.job) {
-                    BarMark(
-                        xStart: .value("Start Time", $0.start),
-                        xEnd: .value("End Time", $0.end),
-                        y: .value("Job", $0.job)
-                    )
-                }
-                .frame(height: 200)
-                .padding()
-                
-            }
+            
             
             
         }
