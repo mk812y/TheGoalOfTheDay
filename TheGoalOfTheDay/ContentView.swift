126a4f039810c6c65fbb37b3d08e78296dc289fe
mk812y <mk812y@gmail.com>
2023-01-25 21:51:35 +0300

    allColor

--- TheGoalOfTheDay/ContentView.swift
+++ TheGoalOfTheDay/ContentView.swift
@@ -22,11 +22,86 @@ struct Job: Identifiable {
 }
 
 struct ContentView: View {
-    @State private var progress = 0.6    
+    @State private var progress = 0.6
     
     var body: some View {
         ZStack {
-            Color("1g")
+//            Color
+            VStack {
+                HStack {
+                    Circle()
+                        .fill(Color(.systemRed)) //#ff3b30
+                    Circle()
+                        .fill(Color(.systemOrange))
+                    Circle()
+                        .fill(Color(.systemYellow))
+                    Circle()
+                        .fill(Color(.systemGreen))
+                    Circle()
+                        .fill(Color(.systemMint))
+                    Circle()
+                        .fill(Color(.systemTeal))
+                }
+                HStack {
+                    Circle()
+                        .fill(Color(.systemBrown)) //#ff3b30
+                    Circle()
+                        .fill(Color(.systemPink))
+                    Circle()
+                        .fill(Color(.systemPurple))
+                    Circle()
+                        .fill(Color(.systemIndigo))
+                    Circle()
+                        .fill(Color(.systemBlue))
+                    Circle()
+                        .fill(Color(.systemCyan))
+                }
+                ZStack {
+                    Rectangle()
+                        .fill(Color("2g"))
+                        .frame(height: 200)
+                    Text("fs")
+                        .foregroundColor(.red)
+                }
+                HStack {
+                    Circle()
+                        .fill(Color("1"))
+                    Circle()
+                        .fill(Color("2"))
+                    Circle()
+                        .fill(Color("3"))
+                    Circle()
+                        .fill(Color("4"))
+                    Circle()
+                        .fill(Color("5"))
+                }
+                HStack {
+                    Circle()
+                        .fill(Color("1g"))
+                    Circle()
+                        .fill(Color("2g"))
+                    Circle()
+                        .fill(Color("3g"))
+                    Circle()
+                        .fill(Color("4g"))
+                    Circle()
+                        .fill(Color("5g"))
+                }
+                HStack {
+                    Circle()
+                        .fill(Color("dust"))
+                    Circle()
+                        .fill(Color("peachDust"))
+                    Circle()
+                        .fill(Color("peach"))
+                    Circle()
+                        .fill(Color("greenDust"))
+                    Circle()
+                        .fill(Color("green"))
+                }
+                
+                
+            }
             
             
             
