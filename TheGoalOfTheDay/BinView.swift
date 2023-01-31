feb6570dc909d6a918278fc79e1f0d1c974dc0b1
mk812y <mk812y@gmail.com>
2023-01-31 20:46:49 +0300

    все херня

--- TheGoalOfTheDay/BinView.swift
+++ TheGoalOfTheDay/BinView.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct BinView: View {
+    @State private var rotation = -90.0
+    //100 / 1440 (24 hours * 60 minutes) = 0.069
+    //1440 / 3 = 480 - 480 * 0.069 = 1 minutes
+
     var body: some View {
-        Gauge(value: 60, in: 10...180) {
-        } currentValueLabel: {
-            Text("60")
-        } minimumValueLabel: {
-            Text("10")
-        } maximumValueLabel: {
-            Text("180")
-        }
-        .gaugeStyle(.accessoryCircular)
+        VStack {
+            ZStack {
+                CircleBaseView()
+                CicleProgressLine(color: .blue, from: 0.0, to: 0.3312, rotation: rotation)
+                CicleProgressLine(color: .red, from: 0.3312, to: 0.6624, rotation: rotation)
+                CicleProgressLine(color: .yellow, from: 0.6624, to: 1.0, rotation: rotation)
+//                    CicleProgressLine(color: .yellow, from: 0.75, to: 0.85, rotation: rotation)
+//                    CicleProgressLine(color: .blue, from: 0.89, to: 0.98, rotation: rotation)
+            }
+            Gauge(value: 60, in: 10...180) {
+            } currentValueLabel: {
+                Text("60")
+            } minimumValueLabel: {
+                Text("10")
+            } maximumValueLabel: {
+                Text("180")
+            }
+            .gaugeStyle(.accessoryCircular)
         .scaleEffect(6.5)
+        }
     }
 }
 
--- TheGoalOfTheDay/ContentView.swift
+++ TheGoalOfTheDay/ContentView.swift
@@ -14,22 +14,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var rotation = -90.0
-    //100 / 1440 (24 hours * 60 minutes) = 0.069
-    //1440 / 3 = 480 - 480 * 0.069 = 1 minutes
     
     var body: some View {
         ZStack {
             Color(.systemGray5)
             VStack {
-                ZStack {
-                    CircleBaseView()
-                    CicleProgressLine(color: .blue, from: 0.0, to: 0.3312, rotation: rotation)
-                    CicleProgressLine(color: .red, from: 0.3312, to: 0.6624, rotation: rotation)
-                    CicleProgressLine(color: .yellow, from: 0.6624, to: 1.0, rotation: rotation)
-//                    CicleProgressLine(color: .yellow, from: 0.75, to: 0.85, rotation: rotation)
-//                    CicleProgressLine(color: .blue, from: 0.89, to: 0.98, rotation: rotation)
-                }
+                
                 ColorExample()
             }
             
